1555b0e345f06bf3fe1de50cc3f7020b787b2798
Lagrange1813 <lagrange1813@163.com>
2021-11-02 08:17:34 +0800

    2021.11.2

---
+++ Cat Viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cat Viewer
+//
+//  Created by Lagrange1813 on 2021/11/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
