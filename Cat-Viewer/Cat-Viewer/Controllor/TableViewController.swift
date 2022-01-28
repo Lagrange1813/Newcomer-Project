14caca1be8c5088d65e50b7fa27b85ea555fc36e
Lagrange1813 <Lagrange1813@163.com>
2022-01-28 16:57:16 +0800

    auto commit

--- Cat-Viewer/Cat-Viewer/Controllor/TableViewController.swift
+++ Cat-Viewer/Cat-Viewer/Controllor/TableViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    
     lazy var dataSource = configureDataSource()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +22,7 @@ class TableViewController: UITableViewController {
         dataSource.apply(snapshot, animatingDifferences: false)
     }
     
-    var cats:[Cat] = [
+    var cats: [Cat] = [
         Cat(name: "这是第1张猫猫", step: "这是第1张猫猫", image: "001"),
         Cat(name: "这是第2张猫猫", step: "这是第2张猫猫", image: "002"),
         Cat(name: "这是第3张猫猫", step: "这是第3张猫猫", image: "003"),
@@ -31,14 +30,14 @@ class TableViewController: UITableViewController {
         Cat(name: "这是第5张猫猫", step: "这是第5张猫猫", image: "005"),
         Cat(name: "这是第6张猫猫", step: "这是第6张猫猫", image: "006"),
         Cat(name: "这是第7张猫猫", step: "这是第7张猫猫", image: "007")
-        ]
+    ]
     
-    func configureDataSource() -> UITableViewDiffableDataSource<Section, Cat>{
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, Cat> {
         let cellIdentifier = "datacell"
         
         let dataSource = DiffableDataSource(
             tableView: tableView,
-            cellProvider: {  tableView, indexPath, cat in
+            cellProvider: { tableView, indexPath, cat in
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! TableViewCell
                 
                 cell.nameLabel.text = cat.name
@@ -54,28 +53,32 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         // Get the selected cat
-        guard let cat = self.dataSource.itemIdentifier(for: indexPath)
+        guard let cat = dataSource.itemIdentifier(for: indexPath)
         else {
             return UISwipeActionsConfiguration()
         }
+        
         // Delete
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, completionHandler in
             var snapshot = self.dataSource.snapshot()
             snapshot.deleteItems([cat])
             self.dataSource.apply(snapshot, animatingDifferences: true)
             completionHandler(true)
         }
+        
         // Share
-        let shareAction = UIContextualAction(style: .normal, title: "Share") { (action, sourceView, completionHandler) in
+        let shareAction = UIContextualAction(style: .normal, title: "Share") { _, _, completionHandler in
             let defaultText = "看看这只猫猫！"
             let activityController: UIActivityViewController
             if let imageToShare = UIImage(named: cat.image) {
-                activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil) }else {
+                activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+            } else {
                 activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
-        }
+            }
             self.present(activityController, animated: true, completion: nil)
             completionHandler(true)
         }
+        
         // Configure both actions as swipe action
         let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
         return swipeConfiguration
@@ -85,9 +88,8 @@ class TableViewController: UITableViewController {
         if segue.identifier == "showCatDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
                 let destinationController = segue.destination as! DetailViewController
-                destinationController.catImageName = self.cats[indexPath.row].image
+                destinationController.catImageName = cats[indexPath.row].image
             }
         }
     }
-    
 }
